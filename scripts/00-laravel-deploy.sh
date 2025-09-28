70c87bc1e042074fb3b1d9a72cb15dcff8a78d5d
Hunter Cox <huntersamuelcox1992@gmail.com>
2025-09-28 01:30:57 -0400

    update deploy script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer install --no-dev --working-dir=/var/www/html
+composer install --no-dev --working-dir=/var/www/html --no-interaction --prefer-dist --optimize-autoloader
+
+echo "Installing Node deps..."
+cd /var/www/html
+npm ci --no-audit --no-fund
+
+echo "Building assets..."
+npm run build
 
 echo "Caching config..."
 php artisan config:cache
